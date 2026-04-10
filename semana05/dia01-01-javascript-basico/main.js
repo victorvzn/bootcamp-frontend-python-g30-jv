8741b75c3e9be8f62b6ebff022c99026276385f8
Victor Villazón <victorzn23@gmail.com>
2026-04-09 19:48:05 -0500

    semana05, js basico

--- semana05/dia01-01-javascript-basico/main.js
+++ semana05/dia01-01-javascript-basico/main.js
@@ -1,1 +1,16 @@
+// La siguiente líne imprime en la consola del navegador el texto Hola Javascript
+
 console.log('¡Hola Javascript!')
+
+// 1. COMENTARIOS, Javascript ignora estas líneas y sirve para explicar el código.
+
+// Esto es un comentario de una línea
+
+/*
+  Esto es un comentario
+  de 
+  varias
+  líneas
+*/
+
+
