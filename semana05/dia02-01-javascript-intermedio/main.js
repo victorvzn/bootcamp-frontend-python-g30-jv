68d220e8cf5321320c57e8681875d5b49e5c7a24
Victor Villazón <victorzn23@gmail.com>
2026-04-10 19:40:29 -0500

    semana02, javascript intermedio - arrays y objects

---
+++ semana05/dia02-01-javascript-intermedio/main.js
@@ -0,0 +1,19 @@
+// console.log('Hola JS')
+
+// TIPOS DE DATOS PRIMITIVOS (Number, String, Boolean, null, undefined)
+
+// TIPOS DE DATOS NO PRIMITIVOS (Arrays, Objects)
+
+// ARRAYS
+
+// Un arreglo pueden contener elementos de cualquier tipo de dato: cadena, numeros, boolean, null, arrays, objetos, etc.)
+
+// DECLARACIÓN
+
+const arregloVacio = []
+let listaDeNumeros = [25, 36, 89, 15.60]
+const listaDeValores = [1, 2, 3, 'Victor', 'Villazón', 'Código', true, null, undefined]
+
+console.log(arregloVacio)
+console.log(listaDeNumeros)
+console.log(listaDeValores)
