ea73e1a4c326f56b952439924747af7484a942a1
Victor Villazón <victorzn23@gmail.com>
2026-04-17 21:46:58 -0500

    semana06, dia 02, js dom

--- semana06/dia01-02-javascript-dom/04-js-random-emoji/main.js
+++ semana06/dia01-02-javascript-dom/04-js-random-emoji/main.js
@@ -4,9 +4,12 @@ console.log(Math.floor(Math.random() * 3)) // Nos quedamos con la parte entera u
 const emojiList = ['🍕','🍔','🍟','🌭','🍿','😁','🤣','😂','😀','🤗','😎','😣','🙄','😯','🥰','😗','😲','🤑','😤','🤪','😵','🤢','🤬','😱','💀','👾','🤧','🤡','💀','👽','💩']
 
 const elemento = document.querySelector('section')
-
-const indiceAleatorio = Math.floor(Math.random() * emojiList.length)
-
-elemento.textContent = emojiList[indiceAleatorio]
+const boton = document.querySelector('button')
 
 // TODO: Al hacer click en boton 'Generar emoji' actualiza el section de la página con un nuevo emoji
+
+boton.addEventListener('click', function(event) {
+  const indiceAleatorio = Math.floor(Math.random() * emojiList.length)
+
+  elemento.textContent = emojiList[indiceAleatorio]
+})
