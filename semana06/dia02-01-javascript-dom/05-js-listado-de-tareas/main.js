e40ba2e7d91859621f6406bd28f5eb823ead9fc3
Victor Villazón <victorzn23@gmail.com>
2026-04-17 22:36:59 -0500

    semana06, dia 02, js dom

--- semana06/dia02-01-javascript-dom/05-js-listado-de-tareas/main.js
+++ semana06/dia02-01-javascript-dom/05-js-listado-de-tareas/main.js
@@ -1,1 +1,15 @@
-console.log('hola js')
+// TODO: Obtener la referencia a los elementos con las clases 'task__input', 'task__add', 'task__list'. Deben imprimirlos en la consola.
+
+const taskInput = document.querySelector('.task__input')
+const taskAdd = document.querySelector('.task__add')
+const taskList = document.querySelector('.task__list')
+
+taskAdd.addEventListener('click', function(event) {
+  // Se ejecutará cuando hagamos click en el botón "Añadir tarea"
+  console.log('click!', taskInput.value)
+
+  // Crear elementos dinamicamente
+  // const boton = document.createElement('button')
+  // boton.textContent = 'Este es mi botón'
+  // document.body.appendChild(boton)
+})
