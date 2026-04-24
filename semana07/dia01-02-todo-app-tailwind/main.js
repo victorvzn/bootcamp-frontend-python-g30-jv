ce133f6add555a0e4e9c5f616be4ead795aba3eb
Victor Villazón <victorzn23@gmail.com>
2026-04-23 23:03:20 -0500

    semana07, dia 01, js es6

--- semana07/dia01-02-todo-app-tailwind/main.js
+++ semana07/dia01-02-todo-app-tailwind/main.js
@@ -2,3 +2,4 @@ const taskInput = document.querySelector('.task__input')
 const taskClear = document.querySelector('.task__clear')
 const taskList = document.querySelector('.task__list')
 
+// TODO: Al presionar el botón enter en la caja de texto capturar el texto y añadirlo al url(.task__list)
