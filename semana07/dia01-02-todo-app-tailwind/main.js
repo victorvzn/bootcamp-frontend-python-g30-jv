bc4964c91b58101a4e4a6c0c6e411f8ae9b69b32
Victor Villazón <victorzn23@gmail.com>
2026-04-24 19:26:06 -0500

    semana07, dia 02, js todo app

--- semana07/dia01-02-todo-app-tailwind/main.js
+++ semana07/dia01-02-todo-app-tailwind/main.js
@@ -2,4 +2,11 @@ const taskInput = document.querySelector('.task__input')
 const taskClear = document.querySelector('.task__clear')
 const taskList = document.querySelector('.task__list')
 
-// TODO: Al presionar el botón enter en la caja de texto capturar el texto y añadirlo al url(.task__list)
+// TODO: Al presionar el botón enter en la caja de texto capturar el texto y añadirlo al ul(.task__list)
+
+taskInput.addEventListener('keydown', function(event) {
+  
+  if (event.key === 'Enter') {
+    console.log(event.key)
+  }
+})
