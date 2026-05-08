24cd8e8687a2f70347d6931a1643f5f4fe33bef6
Victor Villazón <victorzn23@gmail.com>
2026-05-07 20:45:53 -0500

    semana08, dia 01, js asincronia http peticiones fetch

--- semana08/dia01-01-js-asincronia-http-peticiones-fetch/main.js
+++ semana08/dia01-01-js-asincronia-http-peticiones-fetch/main.js
@@ -6,10 +6,12 @@ console.log(fetch(url)) // Promise {<pending>} -> Devuelve una promesa (Promise
 
 // Estados de una promesa -> pending, fulfilled(OK) y rejected (Falló)
 
-fetch(url)
-  .then(response => response.json()) // Se ejecuta cuando la respuesta llegó correctamente y además convertimos la respuesta a un objeto JS
-  .then(data => {
-    console.log(data) // Aquí podemos usar el objeto JS
-    console.log(data[0])
-    console.log(data[0].title)
-  })
+// fetch(url)
+//   .then(response => response.json()) // Se ejecuta cuando la respuesta llegó correctamente y además convertimos la respuesta a un objeto JS
+//   .then(data => {
+//     console.log(data) // Aquí podemos usar el objeto JS
+//     console.log(data[0])
+//     console.log(data[0].title)
+//   })
+
+// TODO: Renderizar la lista de posts del servicio jsonplaceholder con sus campos título, id y body
