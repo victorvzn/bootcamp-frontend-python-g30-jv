bb14cd5d8e51ad87cde0751119387bc0d37db282
Victor Villazón <victorzn23@gmail.com>
2026-05-07 21:01:52 -0500

    semana08, dia 01, js asincronia http peticiones fetch

--- semana08/dia01-01-js-asincronia-http-peticiones-fetch/main.js
+++ semana08/dia01-01-js-asincronia-http-peticiones-fetch/main.js
@@ -15,3 +15,29 @@ console.log(fetch(url)) // Promise {<pending>} -> Devuelve una promesa (Promise
 //   })
 
 // TODO: Renderizar la lista de posts del servicio jsonplaceholder con sus campos título, id y body
+
+const renderPosts = (posts = []) => {
+  const divApp = document.querySelector('#app')
+
+  let postsList = ''
+
+  posts.forEach(post => {
+    postsList += `<h2>${post.id} - ${post.title}</h2>`
+  })
+
+  divApp.innerHTML = postsList
+}
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+    renderPosts(data)
+  })
+  .catch(error => {
+    // Sirve para controlar errores inesperados
+    console.log(error)
+  })
+
+// TODO: Renderizar la lista de todos del servicio jsonplaceholder con su titulo, id
+// URL: https://jsonplaceholder.typicode.com/todos
