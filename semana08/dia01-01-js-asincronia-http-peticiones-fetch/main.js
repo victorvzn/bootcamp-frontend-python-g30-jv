c499feeaf96f44bdd848eb3da77826b752781ec4
Victor Villazón <victorzn23@gmail.com>
2026-05-07 20:41:53 -0500

    semana08, dia 01, js asincronia http peticiones fetch

--- semana08/dia01-01-js-asincronia-http-peticiones-fetch/main.js
+++ semana08/dia01-01-js-asincronia-http-peticiones-fetch/main.js
@@ -1,1 +1,15 @@
-console.log('Hola JS')
+// OBJETIVO: Consultar información en formato JSON desde un servicio web y su URL
+
+const url = 'https://jsonplaceholder.typicode.com/posts'
+
+console.log(fetch(url)) // Promise {<pending>} -> Devuelve una promesa (Promise)
+
+// Estados de una promesa -> pending, fulfilled(OK) y rejected (Falló)
+
+fetch(url)
+  .then(response => response.json()) // Se ejecuta cuando la respuesta llegó correctamente y además convertimos la respuesta a un objeto JS
+  .then(data => {
+    console.log(data) // Aquí podemos usar el objeto JS
+    console.log(data[0])
+    console.log(data[0].title)
+  })
