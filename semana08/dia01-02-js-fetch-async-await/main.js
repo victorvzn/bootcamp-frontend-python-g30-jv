745b21cf597d530079fab71baac0358b657fe36c
Victor Villazón <victorzn23@gmail.com>
2026-05-07 22:00:58 -0500

    semana08, dia 01, js asincronia http peticiones fetch

--- semana08/dia01-02-js-fetch-async-await/main.js
+++ semana08/dia01-02-js-fetch-async-await/main.js
@@ -1,1 +1,10 @@
 const url = 'https://jsonplaceholder.typicode.com/users'
+
+fetch(url)
+  .then(respuesta => respuesta.json())
+  .then(data => {
+    console.log(data)
+  })
+  .catch(error => {
+    console.log(error)
+  })
