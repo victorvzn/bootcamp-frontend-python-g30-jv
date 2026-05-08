3e6a275a4e917c3dcf2decd35ad61dab4de7b587
Victor Villazón <victorzn23@gmail.com>
2026-05-07 22:43:09 -0500

    semana08, dia 01, js asincronia http peticiones fetch

--- semana08/dia01-02-js-fetch-async-await/main.js
+++ semana08/dia01-02-js-fetch-async-await/main.js
@@ -29,5 +29,24 @@ const fetchUsersConRetorno = async () => { // Retorna un promesa (Promise)
 //   .then(users => console.log(users))
 
 const fetchUsersConManejoDeErrores = async () => {
-  
+  try {
+    const response = await fetch(url)
+
+    console.log(response.status) // 200
+
+    if (response.status === 404) {
+      console.log('Tuvimos problemas para cargar el recurso users')
+      // return
+      throw new Error('ERROR HTTP:' + response.status)
+    }
+
+    return await response.json()
+  } catch (error) { // Manejamos errores inesperados como ausencia de internet
+    console.log(error)
+  }
 }
+
+fetchUsersConManejoDeErrores()
+  .then(users => {
+    console.log(users)
+  })
