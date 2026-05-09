e1182624304663b8d838a1d655d2232e1d678a68
Victor Villazón <victorzn23@gmail.com>
2026-05-08 21:57:11 -0500

    semana08, dia 02, js fetch pokemons pagination localstorage

--- semana08/dia02-01-js-fetch-pokemons-pagination-localstorage/main.js
+++ semana08/dia02-01-js-fetch-pokemons-pagination-localstorage/main.js
@@ -1,4 +1,4 @@
-// TODO: 01 - Crear una función que nos permita consultar los datos de la pokeapi.co
+// DONE: 01 - Crear una función que nos permita consultar los datos de la pokeapi.co
 
 const fetchPokemons = async () => {
   const API_URL = 'https://pokeapi.co/api/v2/pokemon'
@@ -7,18 +7,42 @@ const fetchPokemons = async () => {
 
   const data = await response.json() // Devuelve una promesa
 
-  return data
+  // Obtener el id en base al campo url
+
+  const dataResults = data.results.map(pokemon => {
+    const id = pokemon.url.split('/').at(6)
+    const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+
+    return {
+      ...pokemon,
+      id,
+      image
+    }
+  })
+
+  return {
+    ...data,
+    results: dataResults
+  }
 }
 
-// TODO: 02 - Crear una función que nos permita renderizar(imprimir) los pokemons en la página
+// DONE: 02 - Crear una función que nos permita renderizar(imprimir) los pokemons en la página
 const renderPokemons = (pokemons = []) => {
   const pokemonList = document.querySelector('#pokemonList')
 
   let elements = ''
 
-  // TODO: Necesitamos renderizar el nombre del pokemon en una etiqueta article en el elemento section con id pokemonList
+  // DONE: Necesitamos renderizar el nombre del pokemon en una etiqueta article en el elemento section con id pokemonList
+
+  // TODO: Necesitamos agrega una imagen a cada pokemon
+
   pokemons.forEach(pokemon => {
-    elements += `<article>${pokemon.name}</article>`
+    elements += `
+      <article>
+        ${pokemon.name}
+        <img src="${pokemon.image}" width="80" height="80" />
+      </article>
+    `
   })
 
   pokemonList.innerHTML = elements
@@ -28,5 +52,5 @@ const renderPokemons = (pokemons = []) => {
 fetchPokemons()
   .then(data => {
     console.log(data)
-    renderPokemons(data)
+    renderPokemons(data.results)
   })
