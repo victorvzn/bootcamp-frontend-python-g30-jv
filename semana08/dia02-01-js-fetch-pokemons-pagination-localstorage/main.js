b0c0d102fbb02a41cd7009fda4459863dbcdf51f
Victor Villazón <victorzn23@gmail.com>
2026-05-08 22:44:10 -0500

    semana08, dia 02, js fetch pokemons pagination localstorage

--- semana08/dia02-01-js-fetch-pokemons-pagination-localstorage/main.js
+++ semana08/dia02-01-js-fetch-pokemons-pagination-localstorage/main.js
@@ -1,7 +1,11 @@
+const LIMIT = 6
+
 // DONE: 01 - Crear una función que nos permita consultar los datos de la pokeapi.co
 
-const fetchPokemons = async () => {
-  const API_URL = 'https://pokeapi.co/api/v2/pokemon'
+const fetchPokemons = async (page = 1) => {
+  const OFFSET = (page - 1) * LIMIT
+
+  const API_URL = `https://pokeapi.co/api/v2/pokemon?offset=${OFFSET}&limit=${LIMIT}`
 
   const response = await fetch(API_URL) // Devuelve una promesa
 
