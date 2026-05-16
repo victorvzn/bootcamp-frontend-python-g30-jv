249c25f3606a7ed48ec36df10c00c2829dc6bd51
Victor Villazón <victorzn23@gmail.com>
2026-05-15 20:37:23 -0500

    semana9, dia01, 01, react introduccion

--- semana09/dia01-01-react-introduccion/src/App.jsx
+++ semana09/dia01-01-react-introduccion/src/App.jsx
@@ -6,3 +6,26 @@ function Welcome() {
 
 export default Welcome
 
+// Qué es un componente?
+// * Es una función, devuelve JSX, todo componente en su nombre empieza con mayúscula.
+
+// Partes de un componente:
+// 1. Imports (Si los hay)
+// 2. Lógica (variables, hooks, functions)
+// 3. Return -> JSX
+// 4. Export ( Nos sirve para crear un módulo)
+
+// Reglas básicas de un componente
+// * UN solo elemento padre
+
+// ESM (ECMAScript Modules)
+// Es una forma moderna de importar archivos, exportar archivos, compartir códifo entre archivos
+// Gracias a esto podemos dividir el proyecto en partes pequeñas y organzadas.
+
+import frutas from './modules.js' // Estamos trayendo la constante frutas desde otro archivo
+
+console.log(frutas) // (3) ['manzanas', 'plátanos', 'peras']
+
+import { curso } from './modules.js'
+
+console.log(curso) // {name: 'Algoritmos', nota: 20}
---
+++ semana09/dia01-01-react-introduccion/src/modules.js
@@ -0,0 +1,10 @@
+const frutas = ['manzanas', 'plátanos', 'peras']
+
+// Exportación por defecto
+export default frutas
+
+// Exportación nombrado
+export const curso = {
+  name: 'Algoritmos',
+  nota: 20
+}
