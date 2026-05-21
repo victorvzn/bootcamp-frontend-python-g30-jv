5d5a742b4c29f899a44e2800d24152d5b935b2ca
Victor Villazón <victorzn23@gmail.com>
2026-05-20 21:06:33 -0500

    semana9, dia02, 03, react eventos useState hooks

--- semana09/dia02-03-react-eventos-usestate-hooks/src/App.jsx
+++ semana09/dia02-03-react-eventos-usestate-hooks/src/App.jsx
@@ -1,3 +1,17 @@
+const Contador = () => {
+  return (
+    <div className="bg-yellow-400 p-4 m-4">
+      <h1 className="text-center text-2xl">Contador</h1>
+
+      <div className="flex gap-4 justify-center">
+        <button className="bg-blue-400 px-4 py-3 cursor-pointer hover:bg-blue-600">+1</button>
+        <span className="bg-slate-400 px-8 py-3">0</span>
+        <button className="bg-blue-400 px-4 py-3 cursor-pointer hover:bg-blue-600">-1</button>
+      </div>
+    </div>
+  )
+}
+
 const App = () => {
   return (
     <div>
@@ -6,6 +20,7 @@ const App = () => {
       </h1>
 
       {/* TODO: Crear un archivo llamado Contador.jsx. Y usarlo en App.jsx. Añadir dos botones y un span para crear nuestro contador.  */}
+      <Contador />
     </div>
   )
 }
