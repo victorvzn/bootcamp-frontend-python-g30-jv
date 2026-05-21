4c82e3d9ef1834eece3deef6ec4fcc0db52e7656
Victor Villazón <victorzn23@gmail.com>
2026-05-20 19:58:10 -0500

    semana9, dia02, 02, react  css tailwindcss children

--- semana09/dia02-02-react-css-tailwindcss-children/src/App.jsx
+++ semana09/dia02-02-react-css-tailwindcss-children/src/App.jsx
@@ -1,6 +1,8 @@
+import './Card.css'
+
 function Card() {
   return (
-    <div className="card">
+    <div className="card" style={{ backgroundColor: 'yellow', padding: '4px' }}>
       <h1 className="title">Hola React!</h1>
 
       <p className="description">
