ece6b4ba38093c9b0b364a22ed67183fee3502eb
Victor Villazón <victorzn23@gmail.com>
2026-05-20 19:44:11 -0500

    semana9, dia02, 02, react  css tailwindcss children

--- semana09/dia02-02-react-css-tailwindcss-children/src/App.jsx
+++ semana09/dia02-02-react-css-tailwindcss-children/src/App.jsx
@@ -1,6 +1,22 @@
+function Card() {
+  return (
+    <div className="card">
+      <h1 className="title">Hola React!</h1>
+
+      <p className="description">
+        Estoy aprendiendo estilos en React
+      </p>
+
+      <button className="button">
+        OK
+      </button>
+    </div>
+  )
+}
+
 const App = () => {
   return (
-    <div>App</div>
+    <Card />
   )
 }
 
