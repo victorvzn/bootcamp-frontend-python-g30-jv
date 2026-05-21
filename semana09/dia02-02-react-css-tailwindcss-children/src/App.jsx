deca244aa09a10112efa44762424e204aa48a344
Victor Villazón <victorzn23@gmail.com>
2026-05-20 20:17:26 -0500

    semana9, dia02, 02, react  css tailwindcss children

--- semana09/dia02-02-react-css-tailwindcss-children/src/App.jsx
+++ semana09/dia02-02-react-css-tailwindcss-children/src/App.jsx
@@ -16,9 +16,29 @@ function Card() {
   )
 }
 
+function CardConTailwindCSS() {
+  return (
+    <div className='bg-orange-500 w-[300px] mt-4 p-4 flex flex-col gap-4'>
+      <h1 className='text-xl font-bold mb-2 text-center'>Hola CardConTailwindCSS!</h1>
+
+      <p className='text-center'>
+        Estoy aprendiendo estilos en React
+      </p>
+
+      <button className='w-full bg-blue-500 p-2'>
+        OK
+      </button>
+    </div>
+  )
+}
+
 const App = () => {
   return (
-    <Card />
+    <>
+      <Card />
+
+      <CardConTailwindCSS />
+    </>
   )
 }
 
