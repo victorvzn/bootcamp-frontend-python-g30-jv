20b5af0bbd11339b8de1b86b91f92d0f89502813
Victor Villazón <victorzn23@gmail.com>
2026-05-22 22:04:05 -0500

    semana10, dia01, 02, student list crud

--- semana10/dia01-02-react-student-list-crud/src/App.jsx
+++ semana10/dia01-02-react-student-list-crud/src/App.jsx
@@ -1,6 +1,63 @@
+import { useState } from "react"
+
 const App = () => {
+  const DEFAULT_STUDENTS = [
+    {
+      id: '1',
+      name: 'Bulma',
+      city: 'Chiclayo'
+    },
+    {
+      id: '2',
+      name: 'Goku',
+      city: 'Trujillo'
+    },
+    {
+      id: '2',
+      name: 'Vegeta',
+      city: 'Lima'
+    }
+  ]
+
+  const [students, setStudents] = useState(DEFAULT_STUDENTS)
+
   return (
-    <div className="text-4xl">App</div>
+    <main>
+      <h1>Student CRUD</h1>
+
+      <form>
+        <label>
+          <span>Name</span>
+          <input
+            type="text"
+            name="name"
+            placeholder="Ex. Victor Villazón"
+            required
+          />
+        </label>
+
+        <label>
+          <span>City</span>
+          <input
+            type="text"
+            name="city"
+            placeholder="Ex. Chiclayo"
+            required
+          />
+        </label>
+
+        <div>
+          <input
+            type="submit"
+            value="Save"
+          />
+          <input
+            type="reset"
+            value="Clear"
+          />
+        </div>
+      </form>
+    </main>
   )
 }
 
