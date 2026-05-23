46dd3a3723ea8650519cfe43132a0bb156359ec1
Victor Villazón <victorzn23@gmail.com>
2026-05-22 20:23:59 -0500

    semana10, dia01, 01, react imc app reto

--- semana10/dia01-01-react-imc-app-reto/src/App.jsx
+++ semana10/dia01-01-react-imc-app-reto/src/App.jsx
@@ -1,6 +1,30 @@
 const App = () => {
   return (
-    <div>App</div>
+    <section className="w-[400px] bg-slate-200 p-4 mt-8 mx-auto rounded-md">
+      <h1 className="text-3xl text-sky-600 text-center font-bold">IMC APP</h1>
+
+      <div className="pt-4">
+        <p>Peso: 0 kg</p>
+        <input
+          type="range"
+          min="50"
+          max="200"
+        />
+      </div>
+
+      <div>
+        <p>Altura: 0cm</p>
+        <input
+          type="range"
+          min="50"
+          max="200"
+        />
+      </div>
+
+      <p className="font-bold">Tu IMC es 0</p>
+
+      <p className="font-bold text-2xl">Estado de IMC: 0</p>
+    </section>
   )
 }
 
