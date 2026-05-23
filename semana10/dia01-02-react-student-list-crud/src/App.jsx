2ee68eb839dc0785c0ce566df6b11c089d81313e
Victor Villazón <victorzn23@gmail.com>
2026-05-22 22:42:30 -0500

    semana10, dia01, 02, student list crud

--- semana10/dia01-02-react-student-list-crud/src/App.jsx
+++ semana10/dia01-02-react-student-list-crud/src/App.jsx
@@ -22,13 +22,14 @@ const App = () => {
   const [students, setStudents] = useState(DEFAULT_STUDENTS)
 
   return (
-    <main>
-      <h1>Student CRUD</h1>
+    <main class="w-96 mx-auto border border-slate-400 rounded-lg mt-6 p-3">
+      <h1 className="text-2xl text-center text-slate-700 font-bold mb-4">Student CRUD</h1>
 
-      <form>
-        <label>
-          <span>Name</span>
+      <form className="flex flex-col gap-4 bg-slate-100 p-3 rounded-lg border">
+        <label className="flex flex-col gap-2">
+          <span className="text-sm font-medium text-slate-900">Name</span>
           <input
+            className="bg-slate-50 border border-slate-300 text-slate-900 text-sm rounded-lg w-full px-3 py-2"
             type="text"
             name="name"
             placeholder="Ex. Victor Villazón"
@@ -36,9 +37,10 @@ const App = () => {
           />
         </label>
 
-        <label>
-          <span>City</span>
+        <label className="flex flex-col gap-2">
+          <span className="text-sm font-medium text-slate-900">City</span>
           <input
+            className="bg-slate-50 border border-slate-300 text-slate-900 text-sm rounded-lg w-full px-3 py-2"
             type="text"
             name="city"
             placeholder="Ex. Chiclayo"
@@ -46,17 +48,32 @@ const App = () => {
           />
         </label>
 
-        <div>
+        <div className="flex gap-4">
           <input
+            class="bg-blue-700 text-white hover:bg-blue-800 font-medium rounded-lg text-sm w-full px-5 py-3 text-center"
             type="submit"
             value="Save"
           />
           <input
+            class="bg-green-700 text-white hover:bg-green-800 font-medium rounded-lg text-sm w-full px-5 py-3 text-center"
             type="reset"
             value="Clear"
           />
         </div>
       </form>
+
+      <h2 className="text-center text-slate-700 font-bold my-4">Student list</h2>
+
+      <section className="mt-4 flex flex-col gap-2">
+        <div className="flex justify-between items-center gap-2 bg-slate-50 px-3 py-2 rounded-lg border">
+          <div className="text-left">Name</div>
+          <div className="text-left">City</div>
+          <div className="flex gap-2">
+            <button>✏</button>
+            <button>❌</button>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
