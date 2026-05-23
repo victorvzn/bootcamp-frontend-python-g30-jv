7fba0d5a2d14d68328af649998f7f72ff12d3d45
Victor Villazón <victorzn23@gmail.com>
2026-05-22 20:50:49 -0500

    semana10, dia01, 01, react imc app reto

--- semana10/dia01-01-react-imc-app-reto/src/App.jsx
+++ semana10/dia01-01-react-imc-app-reto/src/App.jsx
@@ -1,14 +1,25 @@
+import { useState } from "react"
+
 const App = () => {
+  const [peso, setPeso] = useState(80)
+  const [altura, setAltura] = useState(80)
+
+  const handlePeso = (event) => {
+    setPeso(event.target.value)
+  }
+
   return (
     <section className="w-[400px] bg-slate-200 p-4 mt-8 mx-auto rounded-md">
       <h1 className="text-3xl text-sky-600 text-center font-bold">IMC APP</h1>
 
       <div className="pt-4">
-        <p>Peso: 0 kg</p>
+        <p>Peso: {peso} kg</p>
         <input
           type="range"
           min="50"
           max="200"
+          onChange={handlePeso}
+          value={peso}
         />
       </div>
 
