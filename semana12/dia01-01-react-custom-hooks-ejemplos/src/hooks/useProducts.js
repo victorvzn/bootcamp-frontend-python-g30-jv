0ad1a512b92cbad2471de5fc1fc84e4fe21f2301
Victor Villazón <victorzn23@gmail.com>
2026-06-04 20:51:03 -0500

    semana12, dia01, 01, react custom hooks ejemplos

--- semana12/dia01-01-react-custom-hooks-ejemplos/src/hooks/useProducts.js
+++ semana12/dia01-01-react-custom-hooks-ejemplos/src/hooks/useProducts.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 import { fetchProducts } from "../services/products"
 
 export const useProducts = () => {
-  // TODO: Agregar un estado "cargando" para que muestre un mensaje mientras la data está cargando
+  // TODO: Agregar un estado "cargando..." para que muestre un mensaje mientras la data está cargando
 
   const [products, setProducts] = useState([])
   
