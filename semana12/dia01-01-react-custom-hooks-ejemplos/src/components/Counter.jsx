6db1cb845502d27258d256fde4b19a2053d7ffdc
Victor Villazón <victorzn23@gmail.com>
2026-06-04 19:38:24 -0500

    semana12, dia01, 01, react custom hooks ejemplos

--- semana12/dia01-01-react-custom-hooks-ejemplos/src/components/Counter.jsx
+++ semana12/dia01-01-react-custom-hooks-ejemplos/src/components/Counter.jsx
@@ -1,15 +1,38 @@
+import { useState } from "react"
+
 const Counter = () => {
+  const [count, setCount] = useState(0)
+
   return (
     <div>
       <h3>Counter</h3>
 
       <div>
-        <button>-</button>
-        <strong>0</strong>
-        <button>+</button>
+        <button onClick={() => setCount(count - 1)}>-</button>
+        <strong>{count}</strong>
+        <button onClick={() => setCount(count + 1)}>+</button>
       </div>
     </div>
   )
 }
 
 export default Counter
+
+
+// const Counter = () => {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <div>
+//       <h3>Counter</h3>
+
+//       <div>
+//         <button onClick={() => setCount(count - 1)}>-</button>
+//         <strong>{count}</strong>
+//         <button onClick={() => setCount(count + 1)}>+</button>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Counter
