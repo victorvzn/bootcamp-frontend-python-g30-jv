fc2a1a6dd4cbb9ba4f9e798c2cdbb97db2af3133
Victor Villazón <victorzn23@gmail.com>
2026-06-04 20:50:25 -0500

    semana12, dia01, 01, react custom hooks ejemplos

--- semana12/dia01-01-react-custom-hooks-ejemplos/src/hooks/useProducts.js
+++ semana12/dia01-01-react-custom-hooks-ejemplos/src/hooks/useProducts.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react"
 import { fetchProducts } from "../services/products"
 
 export const useProducts = () => {
+  // TODO: Agregar un estado "cargando" para que muestre un mensaje mientras la data está cargando
+
   const [products, setProducts] = useState([])
   
   useEffect(() => {
--- semana12/dia01-01-react-custom-hooks-ejemplos/src/services/products.js
+++ semana12/dia01-01-react-custom-hooks-ejemplos/src/services/products.js
@@ -1,5 +1,5 @@
 export const fetchProducts = async () => {
-  const response = await fetch('https://dummyjson.com/products')
+  const response = await fetch('https://dummyjson.com/products?delay=1000')
 
   return await response.json()
 }
