2968e42bacbd95a8c6341eb988694ba00a8f263a
Victor Villazón <victorzn23@gmail.com>
2026-06-04 19:32:56 -0500

    semana12, dia01, 01, react custom hooks ejemplos

---
+++ semana12/dia01-01-react-custom-hooks-ejemplos/src/components/Counter.jsx
@@ -0,0 +1,15 @@
+const Counter = () => {
+  return (
+    <div>
+      <h3>Counter</h3>
+
+      <div>
+        <button>-</button>
+        <strong>0</strong>
+        <button>+</button>
+      </div>
+    </div>
+  )
+}
+
+export default Counter
