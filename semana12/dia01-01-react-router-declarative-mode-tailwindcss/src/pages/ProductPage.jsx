2d63910096a724526b58e1b7100f52594fdf5029
Victor Villazón <victorzn23@gmail.com>
2026-06-05 19:39:23 -0500

    semana12, dia02, 01, react router declartive mode tailwindcss

--- semana12/dia01-01-react-router-declarative-mode-tailwindcss/src/pages/ProductPage.jsx
+++ semana12/dia01-01-react-router-declarative-mode-tailwindcss/src/pages/ProductPage.jsx
@@ -1,7 +1,11 @@
 import { useParams } from "react-router"
 
+import { getProduct } from '../services/products'
+
 const ProductPage = () => {
   const { id } = useParams()
+
+  // TODO: Imprimir los datos del producto(ej. title, descripción, price, thumbnail 7 y marca) en el return de este producto
 
   return (
     <div>ProductPage {id}</div>
