70142bf4f13f45b72f52aaae2bdad5adb0b7c528
Victor Villazón <victorzn23@gmail.com>
2026-06-05 19:59:39 -0500

    semana12, dia02, 01, react router declartive mode tailwindcss

--- semana12/dia01-01-react-router-declarative-mode-tailwindcss/src/pages/ProductPage.jsx
+++ semana12/dia01-01-react-router-declarative-mode-tailwindcss/src/pages/ProductPage.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router"
 
 import { getProduct } from '../services/products'
@@ -5,10 +6,30 @@ import { getProduct } from '../services/products'
 const ProductPage = () => {
   const { id } = useParams()
 
+  const [product, setProduct] = useState()
+
   // TODO: Imprimir los datos del producto(ej. title, descripción, price, thumbnail 7 y marca) en el return de este producto
 
+  useEffect(() => {
+    getProduct(id)
+      .then(data => setProduct(data))
+  }, [])
+
   return (
-    <div>ProductPage {id}</div>
+    <main>
+
+      <h1>Product detail #{id}</h1>
+
+      <ul>
+        <li>{product.title}</li>
+        <li>{product.description}</li>
+        <li>S/ {product.price}</li>
+        <li>{product.brand}</li>
+        <li><img src={product.thumbnail} /></li>
+      </ul>
+
+      <pre>{JSON.stringify(product, null, 2)}</pre>
+    </main>
   )
 }
 
