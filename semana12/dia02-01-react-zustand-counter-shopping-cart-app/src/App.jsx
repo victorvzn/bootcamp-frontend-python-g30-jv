dd1bd5d89079cc9f138c96131bc37bae389fe4d1
Victor Villazón <victorzn23@gmail.com>
2026-06-05 22:42:12 -0500

    semana12, dia02, 01, react zustand counter shopping caart app tailwindcss

--- semana12/dia02-01-react-zustand-counter-shopping-cart-app/src/App.jsx
+++ semana12/dia02-01-react-zustand-counter-shopping-cart-app/src/App.jsx
@@ -1,12 +1,36 @@
+import { useEffect } from "react"
 import Counter from "./components/Counter"
+import { useState } from "react"
 
 const App = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch('https://dummyjson.com/products')
+
+      return await response.json()
+    }
+
+    fetchProducts()
+      .then(data => setProducts(data.products))
+  }, [])
+
+
   return (
-    <div className="font-bold">
+    <main className="flex flex-col gap-8 p-4">
+
+      <section className="flex gap-2">
+        <ProductList products={products} />
+
+        {/* <ShoppingCart /> */}
+      </section>
       
-      <Counter />
-      <Counter />
-    </div>
+      <section className="flex flex-col gap-4">
+        <Counter />
+        <Counter />
+      </section>
+    </main>
   )
 }
 
